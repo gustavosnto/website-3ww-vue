d97b9587b6770494d403457a283737a92312d069
Gustavo Santos <gustavo.santos1601@outlook.com>
2022-10-17 20:03:02 -0300

    install components

---
+++ src/components/Footer.vue
@@ -0,0 +1,53 @@
+<script setup>
+import Social from './Social.vue';
+</script>
+
+
+<template>
+    <footer class="main">
+        <div class="container">
+            <div class="business-info">
+                <img src="https://3ww.com.br/wp-content/uploads/2022/07/logo-3ww-wp.png" />
+                <Social />
+            </div>
+
+            <nav class="nav-links">
+                <div>
+                    <ul>
+                        <span>Serviços</span>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <span>Parceiros</span>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <span>A 3ww</span>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="copy">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+    </footer>
+</template>
