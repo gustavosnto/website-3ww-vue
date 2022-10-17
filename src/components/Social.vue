6579f91e1e823c6910519c23f355d18e5ba4b407
Gustavo Santos <gustavo.santos1601@outlook.com>
2022-10-17 19:43:11 -0300

    remove all files

--- src/components/Social.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <ul class="social-icons flex">
-        <li><a href=""><i class="ri-instagram-line"></i></a></li>
-        <li><a href=""><i class="ri-facebook-fill"></i></a></li>
-        <li><a href=""><i class="ri-linkedin-fill"></i></a></li>
-        <li><a href=""><i class="ri-whatsapp-line"></i></a></li>
-    </ul>
-</template>
