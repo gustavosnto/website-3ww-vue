5b93f521593a4d977d55d8eb0b8bcb3e4569f1cb
Gustavo Santos <gustavo.santos1601@outlook.com>
2022-10-17 18:54:40 -0300

    add menu mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,18 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+        showMobileMenu: false,
+        };
+    },
+    methods: {
+        showMenu() {
+        this.showMobileMenu = !this.showMobileMenu;
+        },
+    },
+};
+</script>
+
 <template>
     <header class="main">
         <div class="container flex">
@@ -6,16 +21,18 @@
             </div>
 
             <nav class="menu-js">
-                <ul class="flex">
-                    <li>Home</li>
-                    <li>Soluções</li>
-                    <li>Projetos</li>
-                    <li>Blog</li>
-                    <li>Contato</li>
+                <ul class="flex" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Soluções</a></li>
+                    <li><a href="">Projetos</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contato</a></li>
+
+                    <i class="ri-close-fill close-menu" @click="showMenu()"></i>
                 </ul>
                 <a href="#" class="btn-contact">Entre em contato</a>
 
-                <i class="ri-menu-4-fill menu-mobile"></i>
+                <i class="ri-menu-4-fill menu-mobile" @click="showMenu()"></i>
             </nav>
         </div>
     </header>
