be42f5c751a1e5b0e88e337681ef9cde680e92ec
Gustavo Santos <gustavo.santos1601@outlook.com>
2022-10-17 00:25:16 -0300

    ädd a scss and header and footer

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="main">
+        <div class="container flex">
+            <div class="logo">
+                <img src="https://3ww.com.br/wp-content/uploads/2022/07/logo-3ww-wp.png" />
+            </div>
+
+            <nav class="menu-js">
+                <ul class="flex">
+                    <li>Home</li>
+                    <li>Soluções</li>
+                    <li>Projetos</li>
+                    <li>Blog</li>
+                    <li>Contato</li>
+                </ul>
+                <a href="#" class="btn-contact">Entre em contato</a>
+
+                <i class="ri-menu-4-fill menu-mobile"></i>
+            </nav>
+        </div>
+    </header>
+</template>
---
+++ src/components/Social.vue
@@ -0,0 +1,8 @@
+<template>
+    <ul class="social-icons flex">
+        <li><a href=""><i class="ri-instagram-line"></i></a></li>
+        <li><a href=""><i class="ri-facebook-fill"></i></a></li>
+        <li><a href=""><i class="ri-linkedin-fill"></i></a></li>
+        <li><a href=""><i class="ri-whatsapp-line"></i></a></li>
+    </ul>
+</template>
